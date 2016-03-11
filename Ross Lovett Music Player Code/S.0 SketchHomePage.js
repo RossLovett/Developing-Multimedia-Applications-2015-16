f748bafc4b0a6657343620934b22c203271acf60
RossLovett <ross_lovett@live.co.uk>
2016-03-11 15:46:30 +0000

    11/03/2016
    1st Version

---
+++ Ross Lovett Music Player Code/S.0 SketchHomePage.js
@@ -0,0 +1,22 @@
+function setup() {
+
+    createCanvas (700, 700);     // Draw Page 700x700
+    
+}
+
+function draw() {
+  
+    fill(125, 125, 125)          
+    rect(0, 0, 700, 700)         // Backround Grey
+    
+    fill(100)                    // Rectangles Dark Grey
+    
+    rect(200, 100, 300, 300)     // Client Logo Rectangle
+    
+    rect(100, 500, 100, 100)     // Andrew Howes "May Day" Album Art and Button
+    
+    rect(300, 500, 100, 100)     // Baby Gramps "Live at WFMU" Album Art and Button
+    
+    rect(500, 500, 100, 100)     // The Blend "Losing the Game" Album Art and Button
+
+}
---
+++ Ross Lovett Music Player Code/S.1 SketchAndrewHowes.js
@@ -0,0 +1,47 @@
+// Code by Ross Lovett
+
+function setup() {
+
+    createCanvas (700, 700);     // Draw Page 700x700
+    
+}
+
+function draw() {
+  
+    fill(125, 125, 125)          
+    rect(0, 0, 700, 700)         // Backround Grey
+    fill(100)                    // Rectangles Dark Grey
+    
+    
+    rect(50, 50, 250, 250)       // Andrew Howes "May Day" Album Art (Resized)
+    
+    
+    rect(50, 350, 250, 50)       // Andrew Howes "Nine Stones" Track Name (Text) and button to play track when clicked
+    
+    rect(50, 450, 250, 50)       // Andrew Howes "Spinning In A Green Field" Track Name (Text) and button to play track when clicked
+    
+    rect(50, 550, 250, 50)       // Andrew Howes "Stolen Car" Track Name (Text) and button to play track when clicked
+    
+    
+    rect(400, 75, 200, 50)      // Home (Text) Button to navigate to home page
+    
+    rect(400, 150, 75, 50)       // Next (Text) Button to navigate to next artist page
+    
+    rect(525, 150, 75, 50)       // Prev (Text) Button to navigate to previous artist page
+    
+    
+    rect(375, 250, 250, 250)     // Visualiser area
+    
+    
+    rect(400, 550, 75, 50)       // Play (Text) Button to play track
+    
+    rect(525, 550, 75, 50)       // Pause (Text) Button to pause track
+    
+    
+     
+     
+    
+    
+  
+
+}
